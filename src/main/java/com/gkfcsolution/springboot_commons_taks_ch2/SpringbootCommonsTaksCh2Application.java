70c08aba0a27161d7c7ad0687744e619eef0845b
frankguekeng <frankguekeng11@gmail.com>
2025-05-19 17:05:12 +0100

    Adding Log4j2 starter dependency and excluding default starter logging

--- src/main/java/com/gkfcsolution/springboot_commons_taks_ch2/SpringbootCommonsTaksCh2Application.java
+++ src/main/java/com/gkfcsolution/springboot_commons_taks_ch2/SpringbootCommonsTaksCh2Application.java
@@ -1,18 +1,19 @@
 package com.gkfcsolution.springboot_commons_taks_ch2;
 
-import com.gkfcsolution.springboot_commons_taks_ch2.config.AppProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AppProperties.class)
 public class SpringbootCommonsTaksCh2Application {
 
+    private static Logger logger = LoggerFactory.getLogger(SpringbootCommonsTaksCh2Application.class);
 
     public static void main(String[] args) {
 
         SpringApplication.run(SpringbootCommonsTaksCh2Application.class, args);
+        logger.info("CourseTrackerApplication started successfully with Log4j2 configuration");
 
     }
 
