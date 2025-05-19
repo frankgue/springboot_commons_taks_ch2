ada2887375d1b692bfb57294d3cb8a6f3e1a3710
frankguekeng <frankguekeng11@gmail.com>
2025-05-19 17:40:22 +0100

    Validate User Data using Bean Validation

--- src/main/java/com/gkfcsolution/springboot_commons_taks_ch2/SpringbootCommonsTaksCh2Application.java
+++ src/main/java/com/gkfcsolution/springboot_commons_taks_ch2/SpringbootCommonsTaksCh2Application.java
@@ -1,12 +1,20 @@
 package com.gkfcsolution.springboot_commons_taks_ch2;
 
+import com.gkfcsolution.springboot_commons_taks_ch2.model.Course;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Set;
+
 @SpringBootApplication
-public class SpringbootCommonsTaksCh2Application {
+public class SpringbootCommonsTaksCh2Application implements CommandLineRunner {
 
     private static Logger logger = LoggerFactory.getLogger(SpringbootCommonsTaksCh2Application.class);
 
@@ -18,4 +26,15 @@ public class SpringbootCommonsTaksCh2Application {
     }
 
 
+    @Override
+    public void run(String... args) throws Exception {
+        Course course = new Course();
+        course.setId(1);
+        course.setRating(0);
+
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<Course>> violations = validator.validate(course);
+        violations.forEach(courseConstraintViolation -> logger.error("A constraint violation has occurred. Violation details: [{}].", courseConstraintViolation));
+        ;
+    }
 }
